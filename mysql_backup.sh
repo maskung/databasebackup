431ceb3a1f05cd6fe49434f48902d9bc8bb49ada
System Administrator <root@Suphanuts-MacBook-Pro-7.local>
2020-09-29 22:56:54 +0700

    remove mysql_backup.sh

--- mysql_backup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-mysqldump -ucotto -psupervis --add-drop-table --extended-insert --quote-names --databases cotto > /root/cotto_db_backup/mysql.cotto.$(date "+%Y%m%d").sql
-gzip /root/cotto_db_backup/mysql.cotto.$(date "+%Y%m%d").sql
